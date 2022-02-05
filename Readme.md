bcb8921f7ba1565039bc3f55eb4f5135fbba38ae
Oriol Tauleria <oriol.tauleria@holaluz.com>
2022-02-05 18:18:27 +0100

    First commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Introduction
+
+This is a project that downloads information from Att&ck official github repo and processes it a little bit to provide insights to better understand the information of its datasets.
+
+
+
+# Relevant files
+
+## base.r
+
+The file [base.r](base.r) contains the instructions to download and manipulate datasets in order to gather information about the most used "information labels" in all the dataset.
+
+## Att&ck information.pdf
+In [Att&ck information.pdf](Attack_information.pdf)  there is an introduction to Att&ck and the basic information extracted of the DataSet.
---
+++ base.r
@@ -0,0 +1,51 @@
+# 1. Prepare environment
+
+## 1.1 Install required packages
+
+list.of.packages <- c("rjson","dplyr","plyr")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+
+if(length(new.packages)) {
+  install.packages(new.packages)
+} else {
+  print("[info] No new packages are required.")
+}
+
+## 1.2 Initialize required libraries
+
+library("plyr")
+library("rjson")
+library("dplyr")
+
+## 1.3 Download required data-sets
+
+## From URL: https://github.com/mitre-attack/attack-stix-data/raw/master/enterprise-attack/enterprise-attack.json
+
+if(!file.exists("enterprise-attack.json")){
+
+  download.file("https://github.com/mitre-attack/attack-stix-data/raw/master/enterprise-attack/enterprise-attack.json",
+                "enterprise-attack.json")
+} else
+{print("[info] File already exists, skipping download")}
+
+
+## 1.4 Transform json data to df
+
+origin <- fromJSON(file = "enterprise-attack.json" )
+
+# 2 Clean data
+
+raw_object <- origin$objects
+print(paste("[info] The number of elements is:",length(raw_object)))
+
+
+
+## Work withc "metadata"
+
+### Named List with all elements
+headers_list <- unique(unlist(llply(raw_object, function(x) {x = names(x)})))
+names(headers_list) <- unique(unlist(llply(raw_object, function(x) {x = names(x)})))
+ocurrences <- llply(headers_list, function(y) sum(laply(headers_rows,function(x) {x = y %in% x})))
+ocurrences <- ocurrences[order(unlist(ocurrences),decreasing=TRUE)]
+
+print(ocurrences)
